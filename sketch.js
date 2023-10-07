7b3a5b5ce80001a1825deaca2005920921601ed0
Adam Cole <acole9@gmail.com>
2023-10-07 03:51:19 +0100

    working video capture and updated tiles, cleaner code

--- sketch.js
+++ sketch.js
@@ -1,28 +1,12 @@
-// Video ASCII with scaling
-// Marc Duiker, May 2022
-// https://twitter.com/marcduiker
-//
-// Move mouse or tap in X direction to change the ASCII scaling.
-//
-// Based on the excellent tutorial by The Coding Train / Daniel Shiffman, https://thecodingtrain.com/CodingChallenges/166-ascii-image.html, https://youtu.be/55iwMYv8tGI
-
-const density = "∎@$=+-. ";
-
 let video;
-let asciiDiv;
 let videoLoaded;
 
-const videoScaleDownFactor = 10;
+const fr = 30;
+
+let videoScaleDownFactor = 10;
 let initialVideoWidth;
 let initialVideoHeight;
 let showVideo;
-
-const maxStep = 10;
-const minFontSize = 6;
-const maxFontSize = minFontSize * maxStep;
-
-const imageTiles = [];
-const tileCount = 9;
 
 const sliders = [];
 const initialSliderValues = [
@@ -30,10 +14,16 @@ const initialSliderValues = [
 ];
 let sliderText;
 
-let alternateImageTile1;
-let alternateImageTile3;
+let videoScaleSlider;
+let videoScaleSliderText;
+const videoScaleSliderMin = 1;
+const videoScaleSliderMax = 40;
+
+let imageTiles = [];
 
 function setup() {
+  frameRate(fr);
+
   videoLoaded = false;
   showVideo = false;
 
@@ -50,70 +40,29 @@ function setup() {
     createCanvas(initialVideoWidth, initialVideoHeight);
     videoLoaded = true;
   });
-  console.log(video.width, video.height);
-  // video.size(180, 110);
-  createP("Move mouse or tap in X direction to change the ASCII scaling.");
-  asciiDiv = createDiv();
 
+  loadTiles();
   setupSliders();
-
-  imageTiles.push(loadImage("images/0.png"));
-  imageTiles.push(loadImage("images/1.1.png"));
-  // imageTiles.push(loadImage("images/1.2.png"));
-  imageTiles.push(loadImage("images/2.png"));
-  imageTiles.push(loadImage("images/3.png"));
-  imageTiles.push(loadImage("images/4.png"));
-  imageTiles.push(loadImage("images/5.png"));
-  imageTiles.push(loadImage("images/6.png"));
-  imageTiles.push(loadImage("images/7.png"));
-  imageTiles.push(loadImage("images/8.png"));
-
-  alternateImageTile1 = loadImage("images/1.2.png");
-  alternateImageTile2 = loadImage("images/2.1.png");
-  alternateImageTile3 = loadImage("images/3.1.png");
 }
 
 function draw() {
   if (!videoLoaded) return;
 
-  video.loadPixels();
+  try {
+    video.loadPixels();
+  } catch (e) {
+    console.error(`Error loading video pixels: ${e}`);
+  }
+
+  background(255);
   video.hide();
-  // let asciiImage = "";
 
-  // const stepSize = floor(map(mouseX, 0, windowWidth, 1, maxStep));
-  const stepSize = 1;
-  for (let j = 0; j < video.height; j = j + stepSize) {
-    for (let i = 0; i < video.width; i = i + stepSize) {
-      const pixelIndex = (i + j * video.width) * 4;
-      const r = video.pixels[pixelIndex + 0];
-      const g = video.pixels[pixelIndex + 1];
-      const b = video.pixels[pixelIndex + 2];
-      const avg = getGrayScaleColor(r, g, b);
-      const avgNormalized = map(avg, 0, 255, 0, 1);
+  for (let j = 0; j < video.height; j = j + 1) {
+    for (let i = 0; i < video.width; i = i + 1) {
+      const avgNormalized = getGreyScaleNormalized(i, j);
 
-      // const len = density.length;
-      // const charIndex = floor(map(avg, 0, 255, 0, len));
-      const charIndex = getTileIndexFromGrayScale(avgNormalized);
-
-      let img = imageTiles[charIndex];
-
-      if (charIndex == 1) {
-        if ((i + j) % 2 == 0) {
-          img = alternateImageTile1;
-        }
-      }
-
-      if (charIndex == 2) {
-        if ((i + j) % 2 == 0) {
-          img = alternateImageTile2;
-        }
-      }
-
-      if (charIndex == 3) {
-        if ((i + j) % 2 == 0) {
-          img = alternateImageTile3;
-        }
-      }
+      const tileIndex = getTileIndexFromGrayScale(avgNormalized);
+      const img = getTileImgFromIndex(tileIndex, i, j);
 
       image(
         img,
@@ -122,41 +71,43 @@ function draw() {
         videoScaleDownFactor,
         videoScaleDownFactor
       );
-
-      // const c = density.charAt(charIndex);
-      // if (c == " ") asciiImage += "&nbsp;";
-      // else asciiImage += c;
     }
-
-    // asciiImage += "<br/>";
   }
-
-  // const pointSize = map(stepSize, 1, maxStep, minFontSize, maxFontSize);
-  // const lineHeightSize = pointSize * 0.75;
-  // asciiDiv.style("font-size", `${pointSize}pt`);
-  // asciiDiv.style("line-height", `${lineHeightSize}pt`);
-  // asciiDiv.html(asciiImage);
-
-  // if (mouseIsPressed) {
-  //   let c = video.get(0, 0, width, height);
-  //   image(
-  //     c,
-  //     0,
-  //     0,
-  //     initialVideoWidth * videoScaleDownFactor,
-  //     initialVideoHeight * videoScaleDownFactor
-  //   );
-  // }
 }
 
-function mousePressed() {
-  //saveCanvas('video-ascii', 'jpg');
+function keyPressed() {
+  if (key == " ") {
+    saveCanvas(`study_of_perception_${new Date().getTime()}`, "png");
+  }
+}
+
+function loadTiles() {
+  imageTiles.push([loadImage("images/0.svg")]);
+  imageTiles.push([loadImage("images/1.1.svg"), loadImage("images/1.2.svg")]);
+  imageTiles.push([loadImage("images/2.1.svg"), loadImage("images/2.2.svg")]);
+  imageTiles.push([loadImage("images/3.1.svg"), loadImage("images/3.2.svg")]);
+  imageTiles.push([loadImage("images/4.1.svg"), loadImage("images/4.2.svg")]);
+  imageTiles.push([loadImage("images/5.1.svg"), loadImage("images/5.2.svg")]);
+  imageTiles.push([loadImage("images/6.1.svg"), loadImage("images/6.2.svg")]);
+  imageTiles.push([loadImage("images/7.svg")]);
 }
 
 function getGrayScaleColor(r, g, b) {
   // Gray scale based on linear luminance for each color channel:
   // https://en.wikipedia.org/wiki/Grayscale
   return 0.2126 * r + 0.7152 * g + 0.0722 * b;
+}
+
+function getGreyScaleNormalized(i, j) {
+  const pixelIndex = (i + j * video.width) * 4;
+  const r = video.pixels[pixelIndex + 0];
+  const g = video.pixels[pixelIndex + 1];
+  const b = video.pixels[pixelIndex + 2];
+
+  const greyScaleColor = getGrayScaleColor(r, g, b);
+  const greyScaleColorNormalized = map(greyScaleColor, 0, 255, 0, 1);
+
+  return greyScaleColorNormalized;
 }
 
 function getSliderText() {
@@ -170,7 +121,7 @@ function getSliderText() {
 }
 
 function setupSliders() {
-  for (let i = 0; i < tileCount; i++) {
+  for (let i = 0; i < imageTiles.length; i++) {
     slider = createSlider(0, 1, initialSliderValues[i], 0.001);
 
     slider.input(() => {
@@ -180,20 +131,60 @@ function setupSliders() {
     sliders.push(slider);
   }
 
-  sliderText = createP(getSliderText());
+  sliderText = createP();
+  updateSliderText();
+
+  videoScaleSlider = createSlider(
+    videoScaleSliderMin,
+    videoScaleSliderMax,
+    videoScaleDownFactor,
+    1
+  );
+  videoScaleSliderText = createP();
+  updateVideoScaleSliderText();
+
+  videoScaleSlider.input(() => {
+    videoScaleDownFactor = videoScaleSlider.value();
+    video.size(
+      initialVideoWidth / videoScaleDownFactor,
+      initialVideoHeight / videoScaleDownFactor
+    );
+
+    updateVideoScaleSliderText();
+  });
 }
 
 function updateSliderText() {
-  sliderText.html(getSliderText());
+  sliderText.html(`GreyScale thresholds: ${getSliderText()}`);
+}
+
+function updateVideoScaleSliderText() {
+  videoScaleSliderText.html(`video scale: ${videoScaleSlider.value()}`);
 }
 
 function getTileIndexFromGrayScale(grayScaleValue) {
-  for (let i = 0; i < tileCount - 1; i++) {
+  for (let i = 0; i < imageTiles.length - 1; i++) {
     let slider = sliders[i];
     if (grayScaleValue < slider.value()) {
       return i;
     }
   }
 
-  return tileCount - 1;
+  return imageTiles.length - 1;
+}
+
+function getTileImgFromIndex(tileIndex, i, j) {
+  const imgSet = imageTiles[tileIndex];
+
+  if (imgSet.length == 1) {
+    return imgSet[0];
+  }
+
+  const [img1, img2] = imgSet;
+
+  if ((i + j) % 2 == 0) {
+    return img1;
+  }
+
+  return img2;
 }
